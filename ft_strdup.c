cd417dfb18b0a899d184a97e76c8002c740f68f4
naharagu <naharagu@student.42tokyo.jp>
2022-07-22 21:30:41 +0200

    fix

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: naharagu <naharagu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/29 16:13:20 by naharagu          #+#    #+#             */
-/*   Updated: 2022/07/10 10:58:11 by naharagu         ###   ########.fr       */
+/*   Updated: 2022/07/22 21:25:38 by naharagu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_strdup(const char *s1)
 
 	len = ft_strlen(s1);
 	dest = (char *)malloc(len + 1);
-	if (dest == NULL)
+	if (!dest)
 		return (NULL);
 	ft_strlcpy(dest, s1, len + 1);
 	return (dest);
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: naharagu <naharagu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/02 04:57:28 by naharagu          #+#    #+#             */
-/*   Updated: 2022/07/15 19:46:54 by naharagu         ###   ########.fr       */
+/*   Updated: 2022/07/18 22:49:32 by naharagu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,7 @@ void	*ft_memmove(void *dst, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s1);
+
 size_t	ft_strlcat(char *dest, char *src, size_t dstsize);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlen(const char *s);
